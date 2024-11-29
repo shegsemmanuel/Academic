a198d28139dbff3d6d83349a49cab0cb14216503
Emmanuel Adeoluwa <shegsemmanuel8@gmail.com>
2024-11-29 03:58:26 +0100

    change

--- app/components/Banner/Dropdownone.tsx
+++ app/components/Banner/Dropdownone.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { Listbox } from "@headlessui/react"
 import { CheckIcon, ChevronDownIcon } from '@heroicons/react/20/solid'
 import { useState } from "react";
@@ -14,7 +15,15 @@ const course: Coursetype[] = [
 ]
 
 const Dropdownone = () => {
-    const [selected, setSelected] = useState(course[0])
+    // const [selected, setSelected] = useState(course[0]);
+
+    const [selected, setSelected] = useState(options[0]);
+
+    const options = [
+        { id: 1, name: "Option 1" },
+        { id: 2, name: "Option 2" },
+        { id: 3, name: "Option 3" },
+    ];
 
   return (
     <div className="w-full">
@@ -23,19 +32,66 @@ const Dropdownone = () => {
 
             {/* Toggle dropdown */}
         <div className="relative mt-1">
-            <Listbox.Button>
-                <span
-                 className="block truncate text-xl font-semibold"
-                >
+              {/* <Listbox.Options>
+                <span className="block truncate text-xl font-semibold">
                     {selected.name}
                 </span>
                 <span className="pointer-events-none absolute inset-y-0 right-0">
-                    <ChevronDownIcon 
-                      className="h-5 w-5 text-gray-400"  
-                    />
+                    <ChevronDownIcon className="h-5 w-5 text-gray-400" />
                 </span>
-            </Listbox.Button>
+            </Listbox.Options> */}
+
+          {/* <Listbox value={selected} onChange={setSelected}>
+              <div className="relative mt-1">
+                  <Listbox.Button className="block truncate text-xl font-semibold">
+                      {selected.name}
+                  </Listbox.Button>
+                  <Listbox.Options>
+                      {options.map((option, idx) => (
+                          <Listbox.Option
+                              key={idx}
+                              value={option}
+                              className={({ active }) =>
+                                  `relative cursor-pointer select-none ${
+                                      active ? "bg-blue-500 text-white" : "text-gray-900"
+                                  }`
+                              }
+                          >
+                              {option.name}
+                          </Listbox.Option>
+                      ))}
+                  </Listbox.Options>
+              </div>
+          </Listbox> */}
+
+            <div className="relative mt-1">
+                <Listbox.Button className="block truncate text-xl font-semibold">
+                    {selected.name}
+                </Listbox.Button>
+                <Listbox.Options>
+                    {options.map((option) => (
+                        <Listbox.Option
+                            key={option.id}
+                            value={option}
+                            className={({ active }) =>
+                                `relative cursor-pointer select-none ${
+                                    active ? 'bg-blue-500 text-white' : 'text-gray-900'
+                                }`
+                            }
+                        >
+                            {option.name}
+                        </Listbox.Option>
+                    ))}
+                </Listbox.Options>
+                <span className="pointer-events-none absolute inset-y-0 right-0">
+                    <ChevronDownIcon className="h-5 w-5 text-gray-400" />
+                </span>
+            </div>
+
         </div>
+
+
+
       </Listbox>
     </div>
   );
